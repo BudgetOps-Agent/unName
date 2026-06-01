6dc15f101048e30f062a4fec3dfa253d03d6adf2
yerin08 <hyr2663@naver.com>
2026-06-01 23:49:28 +0900

    feat: 회원가입 입력 폼 기본 구조 생성

--- frontend/src/pages/auth/components/signupForm.tsx
+++ frontend/src/pages/auth/components/signupForm.tsx
@@ -1,7 +1,15 @@
-const signupForm = () => {
+const SignupForm = () => {
   return (
-    <div>signupForm</div>
+    <form>
+      <input type="text" placeholder="이름" required/> <br/>
+      <input type="email" placeholder="이메일" required/> <br/>
+      <input type="password" placeholder="비밀번호" required/> <br/>
+      <input type="password" placeholder="비밀번호 확인" required/> <br/>
+      <input type="tel" placeholder="전화번호" required/> <br/>
+      <input type="date" placeholder="생년월일" required/> <br/>
+      <button type="submit">회원가입</button>
+    </form>
   )
 }
 
-export default signupForm
+export default SignupForm
