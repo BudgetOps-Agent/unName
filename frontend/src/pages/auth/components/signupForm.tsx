d278b0f14f902ce8a095bf64e8134e170cbcb906
yerin08 <hyr2663@naver.com>
2026-06-02 15:29:52 +0900

    feat: 입력 폼 useState 세팅 및 이벤트 핸들러 연결

--- frontend/src/pages/auth/components/signupForm.tsx
+++ frontend/src/pages/auth/components/signupForm.tsx
@@ -1,12 +1,30 @@
+import { useState, SubmitEvent } from "react";
+
 const SignupForm = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    console.log({
+      name, email, password, confirmPassword, phone, birthDate
+    })
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="이름" required/> <br/>
-      <input type="email" placeholder="이메일" required/> <br/>
-      <input type="password" placeholder="비밀번호" required/> <br/>
-      <input type="password" placeholder="비밀번호 확인" required/> <br/>
-      <input type="tel" placeholder="전화번호" required/> <br/>
-      <input type="date" placeholder="생년월일" required/> <br/>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)} required/> <br/>
+      <input type="email" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} required/> <br/>
+      <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} required/> <br/>
+      <input type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}required/> <br/>
+      <input type="tel" placeholder="전화번호" value={phone} onChange={(e) => setPhone(e.target.value)} required/> <br/>
+      <input type="date" placeholder="생년월일" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} required/> <br/>
       <button type="submit">회원가입</button>
     </form>
   )
