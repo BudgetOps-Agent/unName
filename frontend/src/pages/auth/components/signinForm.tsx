58e501f02e2f60b6fa3d9012bb8176ecc050bdb2
yerin08 <hyr2663@naver.com>
2026-06-17 00:04:15 +0900

    feat: 로그인 유효성 검사 함수 연결

--- frontend/src/pages/auth/components/signinForm.tsx
+++ frontend/src/pages/auth/components/signinForm.tsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useState, SubmitEvent } from "react";
 import Input from "@/shared/components/input/Input";
 import Button from "@/shared/components/button/Button";
+import {
+    validateEmailFormat,
+    validatePasswordFormat
+} from "../utils/signupValidator";
 
 const SigninForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleSubmit = (e: SubmitEvent) => {
+        e.preventDefault();
+
+        if (!validateEmailFormat(email)) {
+            alert("유효한 이메일 형식이 아닙니다.");
+            return;
+        }
+
+        if (!validatePasswordFormat(password)) {
+            alert("비밀번호는 최소 8자 이상이어야 하며, 문자와 숫자를 포함해야 합니다.");
+            return;
+        }
+
+        console.log(email, password);
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <Input id="email" label="이메일" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/> <br/>
             <Input id="password" label="비밀번호" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/> <br/>
             <Button type="submit" text="로그인" />
