b8621bb7de46dfd2462a990426097e280559b66a
yerin08 <hyr2663@naver.com>
2026-06-24 13:58:42 +0900

    feat: 비밀번호 변경 커스텀 훅 구현

--- frontend/src/pages/auth/hooks/useFindpw.ts
+++ frontend/src/pages/auth/hooks/useFindpw.ts
@@ -1,17 +1,25 @@
 import axios, { AxiosError } from "axios";
 import { 
     requestVerifyUser,
-    responseVerifyUser
+    responseVerifyUser,
+    requestResetPassword,
+    responseResetPassword
 } from "@/types/auth";
 
-interface UseVerifyUserResult {
+interface ExecuteVerifyResult {
     success: boolean;
     status?: number;
     data?: responseVerifyUser;
 }
 
+interface ExecuteResetPasswordResult {
+    success: boolean;
+    status?: number;
+    data?: responseResetPassword;
+}
+
 export const useFindPw = () => {
-    const executeVerify = async (verifyData: requestVerifyUser): Promise<UseVerifyUserResult> => {
+    const executeVerify = async (verifyData: requestVerifyUser): Promise<ExecuteVerifyResult> => {
 
         try {
             const response = await axios.post<responseVerifyUser>('/api/user/verify-user', verifyData);
@@ -29,6 +37,24 @@ export const useFindPw = () => {
             };
         }
     };
+    
+    const executeResetPassword = async (resetPwData: requestResetPassword): Promise<ExecuteResetPasswordResult> => {
+        
+        try {
+            const response = await axios.post<responseResetPassword>('/api/user/reset-password', resetPwData);
 
-    return { executeVerify };
+            return {
+                success: true,
+                data: response.data
+            };
+        } catch (error) {
+            const axiosError = error as AxiosError;
+
+            return {
+                success: false,
+                status: axiosError.response?.status
+            };
+        }
+    }
+    return { executeVerify, executeResetPassword };
 }
