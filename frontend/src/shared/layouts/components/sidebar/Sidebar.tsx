0eaae7457172266a24395a2bd4280afd9de96b86
yerin08 <hyr2663@naver.com>
2026-06-29 13:27:02 +0900

    feat: 사이드바 open/close 토글 기능 추가

--- frontend/src/shared/layouts/components/sidebar/Sidebar.tsx
+++ frontend/src/shared/layouts/components/sidebar/Sidebar.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import logoSvg from "@/assets/logo.svg";
-import dashboardSvg from "@/assets/dashboard.svg";
-import dashboardActiveSvg from "@/assets/dashboard-active.svg";
-import expensesSvg from "@/assets/expenses.svg";
-import expensesActiveSvg from "@/assets/expenses-active.svg";
-import budgetSvg from "@/assets/budget.svg";
-import budgetActiveSvg from "@/assets/budget-active.svg";
-import memberSvg from "@/assets/member.svg";
-import memberActiveSvg from "@/assets/member-active.svg";
-import reportSvg from "@/assets/report.svg";
-import reportActiveSvg from "@/assets/report-active.svg";
+import dashboardSvg from "@/assets/sidebar/dashboard.svg";
+import dashboardActiveSvg from "@/assets/sidebar/dashboard-active.svg";
+import expensesSvg from "@/assets/sidebar/expenses.svg";
+import expensesActiveSvg from "@/assets/sidebar/expenses-active.svg";
+import budgetSvg from "@/assets/sidebar/budget.svg";
+import budgetActiveSvg from "@/assets/sidebar/budget-active.svg";
+import memberSvg from "@/assets/sidebar/member.svg";
+import memberActiveSvg from "@/assets/sidebar/member-active.svg";
+import reportSvg from "@/assets/sidebar/report.svg";
+import reportActiveSvg from "@/assets/sidebar/report-active.svg";
+import toggleIcon from "@/assets/sidebar/sidebar-toggle.svg";
 
 const menus = [
     {
@@ -47,14 +49,19 @@ const menus = [
 
 export default function Sidebar({}) {
 
+    const [isOpen, setIsOpen] = useState(true);
     const pathname = usePathname();
 
+    const toggleSidebar = () => {
+        setIsOpen(prev => !prev);
+    }
+
     return (
-        <aside className="sidebar-container">
+        <aside className={`sidebar-container ${isOpen ? "" : "closed"}`}>
             <div className="logo-section">
                 <Link href="/dashboard" className="logo-btn">
                     <img src={logoSvg.src} alt="logo" />
-                    <span>BudgetOps</span>
+                    {isOpen && <span>BudgetOps</span>}
                 </Link>
             </div>
 
@@ -63,15 +70,31 @@ export default function Sidebar({}) {
                     <Link
                         key={menu.path}
                         href={menu.path}
-                        className={`menu ${pathname === menu.path ? "active" : ""}`}
+                        className={`menu ${pathname === menu.path ? "active" : ""} ${isOpen ? "" : "closed"}`}
                     >
                         <span>
-                            <img src={pathname === menu.path ? menu.activeIcon.src : menu.icon.src} alt="" />
+                            <img src={
+                                pathname === menu.path 
+                                ? menu.activeIcon.src 
+                                : menu.icon.src} 
+                                alt="" 
+                            />
                         </span>
-                        <span>{menu.text}</span>
+                        {isOpen && <span>{menu.text}</span>}
                     </Link>
                 ))}
             </nav>
+
+            <button 
+                className={`sidebar-toggle`}
+                onClick={toggleSidebar}
+            >
+                <img
+                    src={toggleIcon.src} 
+                    alt="sidebar-open" 
+                    className={`${isOpen ? "" : "rotate"}`}
+                />
+            </button>
         </aside>
     )
 }
