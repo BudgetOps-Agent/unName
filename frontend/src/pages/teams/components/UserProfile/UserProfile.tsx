a6ac6b75e6c09574ded9d1163a66ae46177c35ce
yerin08 <hyr2663@naver.com>
2026-07-08 23:44:59 +0900

    feat: 마이페이지 user 정보 카드 컴포넌트 생성

---
+++ frontend/src/pages/teams/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,36 @@
+import styles from './userprofile.module.css';
+import Button from '@/shared/components/button/Button';
+
+const UserProfile = () => {
+    return (
+        <div className={styles.profileContainer}>
+            <div className={styles.profileHeader}>
+                <span className={styles.logo}>김</span>
+                
+                <div>
+                    <p className={styles.name}>김민준</p>
+                    <span className={styles.role}>관리자</span>
+                </div>
+            </div>
+
+            <div className={styles.userInfo}>
+                <div className={styles.infoBox}>
+                    <span>이메일</span>
+                    <p>minjun.kim@hyu.ac.kr</p>
+                </div>
+                <div className={styles.infoBox}>
+                    <span>전화번호</span>
+                    <p>010-1234-5678</p>
+                </div>
+                <div className={styles.infoBox}>
+                    <span>가입일</span>
+                    <p>2024-09-01</p>
+                </div>
+            </div>
+
+            <Button className={styles.modifyBtn} style="secondary" text="프로필 수정" />
+        </div>
+    )
+}
+
+export default UserProfile
