069c84edf26dcd1f65a1abb2b61dd5d8125090d5
hanyiseul <seulli_hys@daum.net>
2026-07-08 22:59:17 +0900

    feat: 아이디찾기 테스트 수정중

--- frontend/src/pages/auth/components/findidForm.tsx
+++ frontend/src/pages/auth/components/findidForm.tsx
@@ -13,10 +13,30 @@ const FindIdForm = () => {
     const [phone, setPhone] = useState('');
 
     const router = useRouter();
+    
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        
+        const number = e.target.value.replace(/\D/g, "");
+        let value = "";
+        if (number.length <= 3) {
+            value = number;
+        } else if (number.length <= 7) {
+            value = number.replace(/(\d{3})(\d+)/, "$1-$2");
+        } else {
+            value = number.replace(/(\d{3})(\d{4})(\d+)/, "$1-$2-$3");
+        }
+
+        setPhone(value);
+    }
 
     const handleSubmit = async (e: SubmitEvent) => {
         e.preventDefault();
 
+        if (!name.trim()) {
+            alert("이름을 입력해주세요.");
+            return;
+        }
+        
         if (!validatePhoneFormat(phone)) {
             alert("유효한 전화번호 형식이 아닙니다.");
             return;
@@ -28,33 +48,20 @@ const FindIdForm = () => {
         };
 
         const result = await executeFindid(findIdData);
-
+        
         if (result.success) {
             alert(`${result.data?.email}`);
             router.push('/auth/signin');
             return;
         }
 
-        switch (result.status) {
-            case 400:
-                alert("요청 형식이 올바르지 않습니다.");
-                break;
-            case 404:
-                alert("일치하는 회원 정보가 없습니다. 입력하신 정보를 다시 확인해 주세요.");
-                break;
-            case 500:
-                alert("서버가 일시적으로 원활하지 않습니다. 잠시 후 다시 시도해 주세요.");
-                break;
-            default:
-                alert("네트워크 연결이 불안정합니다. 인터넷 상태를 확인해 주세요.");
-                break;
-        }
+        alert(result.message || "네트워크 연결이 불안정합니다. 인터넷 상태를 확인해 주세요.");
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <Input id="name" label="이름" type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
-            <Input id="phone" label="전화번호" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} required/>
+            <Input id="phone" label="전화번호" type="tel" value={phone} onChange={handleChange} required/>
             <div className="buttons">
                 <Button type="submit" text="아이디 찾기" size="lg"/>
             </div>
