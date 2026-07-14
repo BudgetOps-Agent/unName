b6361bdd32436d40e166d45c3a479ee0d1ff5b3f
yerin08 <hyr2663@naver.com>
2026-07-14 13:20:42 +0900

    style: Card 컴포넌트 smallCard 스타일 추가

--- frontend/src/shared/components/card/Card.tsx
+++ frontend/src/shared/components/card/Card.tsx
@@ -5,11 +5,12 @@ interface CardProps {
     className?: string;
     noPadding?: boolean;
     children: React.ReactNode;
+    style?: 'smallCard';
 }
 
-export const Card = ({ className, noPadding, children }: CardProps) => {
+export const Card = ({ className, noPadding, children, style }: CardProps) => {
   return (
-    <div className={`${styles.card} ${className} ${noPadding ? styles.noPadding : ''}`}>
+    <div className={`${styles.card} ${className} ${noPadding ? styles.noPadding : ''} ${styles[`${style}`]}`}>
         {children}
     </div>
   )
