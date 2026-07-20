6541917403708989d8e5c2a76b7943b6fe187b5c
yerin08 <hyr2663@naver.com>
2026-07-20 13:42:35 +0900

    feat: 내 모임 목업 화면 완성

--- frontend/src/pages/teams/index.tsx
+++ frontend/src/pages/teams/index.tsx
@@ -1,6 +1,29 @@
+import { useState } from 'react';
 import styles from './teams.module.css';
 import Link from 'next/link';
+import InviteCard from './components/InviteCard/InviteCard';
 import GroupCard from './components/GroupCard/GroupCard';
+
+const inviteInfo = [
+    {
+        id: 1,
+        name: 'AI 연구 동아리',
+        inviter: '이정민',
+        category: '동아리/학생회',
+        member: 12,
+        budget: 2000000,
+        role: '멤버'
+    },
+    {
+        id: 2,
+        name: '사이드 프로젝트 팀',
+        inviter: '최준혁',
+        category: '스터디',
+        member: 6,
+        budget: 500000,
+        role: '총무'
+    },
+]
 
 const groupInfo = [
     { 
@@ -22,6 +45,9 @@ const groupInfo = [
 ];
 
 const Teams = () => {
+
+    const [isHoverd, setIsHoverd] = useState(false);
+
     return (
         <div className={styles.teamsContainer}>
             <div className={styles.teamsHeader}>
@@ -33,10 +59,37 @@ const Teams = () => {
                 <Link className={styles.newTeamBtn} href={`/teams/new`}>+ 새 모임</Link>
             </div>
 
+            {inviteInfo && inviteInfo.length > 0 && (
+                <div className={styles.inviteListSection}>
+                    <p className={styles.inviteAlarm}>
+                        받은 초대
+                        <span className={styles.inviteCount}>{inviteInfo.length}</span>
+                    </p>
+                    
+                    {inviteInfo.map((invitation) => (
+                        <InviteCard key={invitation.id} invitation={invitation} />
+                    ))}
+                </div>
+            )}
+
             <div className={styles.groupListSection}>
                 {groupInfo.map((group) => (
                     <GroupCard key={group.id} group={group} />
                 ))}
+            </div>
+
+            <div className={styles.logoutBtnSection}>
+                <button
+                    className={styles.logoutBtn}
+                    onMouseEnter={() => setIsHoverd(true)}
+                    onMouseLeave={() => setIsHoverd(false)}
+                >
+                    <img 
+                        src={isHoverd ? "/logout-gray-hover.svg" : "/logout-gray.svg"}
+                        alt="로그아웃"
+                    />
+                    <p>로그아웃</p>
+                </button>
             </div>
         </div>
     )
