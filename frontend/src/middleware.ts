6df185fc427dcd4757ffcb7a18f14d11ed04bc50
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-07-29 12:43:11 +0900

    feat: 404 처리

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,22 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    console.log("미들웨어 실행 pathname:", request.nextUrl.pathname);
-    const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-    if (pathname === "/") {
-        const accessToken = request.cookies.get("accessToken");
+  if (pathname === "/") {
+    const accessToken = request.cookies.get("accessToken");
 
-        if (accessToken) {
-            return NextResponse.redirect(new URL("/teams", request.url));
-        } else {
-            return NextResponse.redirect(new URL("/auth/signin", request.url));
-        }
-    }
+    return NextResponse.redirect(
+      new URL(accessToken ? "/teams" : "/auth/signin", request.url)
+    );
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: "/",
+  matcher: "/",
 };
