3c371220d0dffa19e6901e0ac4d89b4ce6658f7e
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-07-29 12:24:30 +0900

    feat: 지출 경로 변경

--- infra/nginx-conf.ps1
+++ infra/nginx-conf.ps1
@@ -46,15 +46,24 @@ http {
 "@
 
 foreach ($service in $services) {
+
+    if ($service.path.StartsWith("^")) {
+        $locationDirective = "location ~ $($service.path)"
+    } else {
+        $locationDirective = "location $($service.path)"
+    }
+
     $configContent += @"
         # --- $($service.name) ---
-        location $($service.path) {
+        $locationDirective {
             proxy_pass http://127.0.0.1:$($service.port);
             proxy_http_version 1.1;
             proxy_set_header Host `$host;
             proxy_set_header X-Real-IP `$remote_addr;
             proxy_set_header X-Forwarded-For `$proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto `$scheme;
+
+            add_header X-Served-By "$($service.name)-$($service.port)" always;
         }
 
 "@
